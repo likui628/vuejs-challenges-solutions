23966eb98e3a31e9195ad6100a2295fae005ccab
Li Kui <90845831+likui628@users.noreply.github.com>
2022-12-31 19:53:10 +0800

    5-watch-family

--- solutions/5-watch-family/App.vue
+++ solutions/5-watch-family/App.vue
@@ -1,45 +1,53 @@
 <script setup lang="ts">
-import { ref, watch } from "vue"
+import { ref, watch } from 'vue';
 
-const count = ref(0)
+const count = ref(0);
 
 /**
  * Challenge 1: Watch once
  * Make sure the watch callback only triggers once
-*/
-watch(count, () => {
-  console.log("Only triggered once")
-})
+ */
+const unwatch = watch(count, () => {
+  console.log('Only triggered once');
+  unwatch();
+});
 
-count.value = 1
-setTimeout(() => count.value = 2)
+count.value = 1;
+setTimeout(() => (count.value = 2));
 
 /**
  * Challenge 2: Watch object
  * Make sure the watch callback is triggered
-*/
+ */
 const state = ref({
   count: 0,
-})
+});
 
-watch(state, () => {
-  console.log("The state.count updated")
-})
+watch(
+  state,
+  () => {
+    console.log('The state.count updated');
+  },
+  { deep: true }
+);
 
-state.value.count = 2
+state.value.count = 2;
 
 /**
  * Challenge 3: Callback Flush Timing
  * Make sure visited the updated eleRef
-*/
+ */
 
-const eleRef = ref()
-const age = ref(2)
-watch(age, () => {
-  console.log(eleRef.value)
-})
-age.value = 18
-
+const eleRef = ref();
+const age = ref(2);
+watch(
+  age,
+  () => {
+    console.log(eleRef.value);
+  },
+  { flush: 'post' }
+);
+age.value = 18;
 </script>
 
 <template>
