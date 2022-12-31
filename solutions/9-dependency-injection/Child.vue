5175eb9490b742cb5dedb41b7483d36246238731
Li Kui <90845831+likui628@users.noreply.github.com>
2022-12-31 20:01:46 +0800

    9-dependency-injection

--- solutions/9-dependency-injection/Child.vue
+++ solutions/9-dependency-injection/Child.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { inject } from 'vue';
 // Add a piece of code to make the `count` value get injected into the child component.
+const count = inject('count');
 </script>
 
 <template>
