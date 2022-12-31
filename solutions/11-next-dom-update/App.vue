55f999d6949a0b9fb7c708d5ef251592f5f75ac0
Li Kui <90845831+likui628@users.noreply.github.com>
2022-12-31 20:30:07 +0800

    11-next-dom-update

--- solutions/11-next-dom-update/App.vue
+++ solutions/11-next-dom-update/App.vue
@@ -1,17 +1,19 @@
 <script setup>
-import { ref } from "vue"
+import { ref, nextTick } from 'vue';
 
-const count = ref(0)
-const counter = ref(null)
+const count = ref(0);
+const counter = ref(null);
 
 function increment() {
-  count.value++
+  count.value++;
 
   /**
    * DOM is not yet updated, how can we make sure that the DOM gets updated
    * Make the output be true
-  */
-  console.log(+counter.value.textContent === 1)
+   */
+  nextTick(() => {
+    console.log(+counter.value.textContent === 1);
+  });
 }
 </script>
 
