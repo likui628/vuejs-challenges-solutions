b3aaeeb24878842790cd7ab16a035105c75f13fa
Li Kui <90845831+likui628@users.noreply.github.com>
2022-12-31 19:40:06 +0800

    4-writable-computed

--- solutions/4-writable-computed/App.vue
+++ solutions/4-writable-computed/App.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
-import { ref, computed } from "vue"
+import { ref, computed } from 'vue';
 
-const count = ref(1)
-const plusOne = computed(() => count.value + 1)
-
+const count = ref(1);
+const plusOne = computed({
+  get() {
+    return count.value + 1;
+  },
+  set(v) {
+    count.value = v - 1;
+  },
+});
 /**
  * Make the `plusOne` writable.
  * So that we can get the result `plusOne` to be 3, and `count` to be 2.
-*/
+ */
 
-plusOne.value++
-
+// plusOne.value = plusOne.value + 1;
+plusOne.value++;
 </script>
 
 <template>
