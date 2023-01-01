dc2245dc9c537ea941cf9c88c42dfed0ec80c2a0
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-01 17:08:25 +0800

    16-until

--- solutions/16-until/App.vue
+++ solutions/16-until/App.vue
@@ -1,35 +1,39 @@
-<script setup lang='ts'>
-import { ref } from "vue"
+<script setup lang="ts">
+import { ref, watch } from 'vue';
 
-const count = ref(0)
+const count = ref(0);
 
 /**
  * Implement the until function
-*/
+ */
 
 function until(initial) {
   function toBe(value) {
-
+    return new Promise((resolve) => {
+      const unwatch = watch(initial, (newVal) => {
+        if (newVal === value) {
+          unwatch();
+          resolve();
+        }
+      });
+    });
   }
 
   return {
     toBe,
-  }
+  };
 }
 
 async function increase() {
-  count.value = 0
+  count.value = 0;
   setInterval(() => {
-    count.value++
-  }, 1000)
-  await until(count).toBe(3)
-  console.log(count.value === 3) // Make sure the output is true
+    count.value++;
+  }, 1000);
+  await until(count).toBe(3);
+  console.log(count.value === 3); // Make sure the output is true
 }
-
 </script>
 
 <template>
-  <p @click="increase">
-    Increase
-  </p>
+  <p @click="increase">Increase</p>
 </template>
