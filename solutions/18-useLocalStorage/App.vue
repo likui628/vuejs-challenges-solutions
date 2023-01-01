0029bc30d825c0674919b0a69e8aee891332f061
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-01 17:37:15 +0800

    18-useLocalStorage

--- solutions/18-useLocalStorage/App.vue
+++ solutions/18-useLocalStorage/App.vue
@@ -1,31 +1,35 @@
-<script setup lang='ts'>
-
-import { ref } from "vue"
+<script setup lang="ts">
+import { ref, unref, watch } from 'vue';
 
 /**
  * Implement the composable function
  * Make sure the function works correctly
-*/
+ */
 function useLocalStorage(key: string, initialValue: any) {
-  const value = ref(initialValue)
+  const value = ref(initialValue);
 
-  return value
+  if (localStorage.getItem('useLocalStorage')) {
+    value.value = localStorage.getItem('useLocalStorage');
+  }
+
+  watch(value, (newVal) => {
+    localStorage.setItem('useLocalStorage', newVal);
+  });
+
+  return value;
 }
 
-const counter = useLocalStorage("counter", 0)
+const counter = useLocalStorage('counter', 0);
 
 // We can get localStorage by triggering the getter:
-console.log(counter.value)
+console.log(counter.value);
 
 // And we can also set localStorage by triggering the setter:
 
-const update = () => counter.value++
-
+const update = () => counter.value++;
 </script>
 
 <template>
   <p>Counter: {{ counter }}</p>
-  <button @click="update">
-    Update
-  </button>
+  <button @click="update">Update</button>
 </template>
