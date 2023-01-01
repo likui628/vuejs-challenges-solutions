34631d25d9ac8431cada7443376cd4deada49fbf
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-01 17:46:08 +0800

    19-v-focus

--- solutions/19-v-focus/App.vue
+++ solutions/19-v-focus/App.vue
@@ -1,24 +1,25 @@
-<script setup lang='ts'>
-import { ref } from "vue"
+<script setup lang="ts">
+import { ref } from 'vue';
 
-const state = ref(false)
+const state = ref(false);
 
 /**
  * Implement the custom directive
  * Make sure the input element focuses/blurs when the 'state' is toggled
  *
-*/
+ */
 
 const VFocus = {
-
-}
+  updated(el, binding, vnode, prevVnode) {
+    el.focus();
+  },
+};
 
 setInterval(() => {
-  state.value = !state.value
-}, 2000)
-
+  state.value = !state.value;
+}, 2000);
 </script>
 
 <template>
-  <input v-focus="state" type="text">
+  <input v-focus="state" type="text" />
 </template>
