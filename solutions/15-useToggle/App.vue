043a99147f1370eac24a0f3d01b1d0774b53ed03
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-01 16:28:53 +0800

    15-useToggle

--- solutions/15-useToggle/App.vue
+++ solutions/15-useToggle/App.vue
@@ -1,20 +1,23 @@
-<script setup lang='ts'>
-
+<script setup lang="ts">
+import { ref } from 'vue';
 /**
  * Implement a composable function that toggles the state
  * Make the function work correctly
-*/
-function useToggle() {
+ */
+function useToggle(init = false) {
+  const state = ref(init);
 
+  const toggle = () => {
+    state.value = !state.value;
+  };
+
+  return [state, toggle];
 }
 
-const [state, toggle] = useToggle(false)
-
+const [state, toggle] = useToggle(false);
 </script>
 
 <template>
   <p>State: {{ state ? 'ON' : 'OFF' }}</p>
-  <p @click="toggle">
-    Toggle state
-  </p>
+  <p @click="toggle">Toggle state</p>
 </template>
