8fd172e35d0eb4cacea28d6da8df6026d1db007f
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-01 14:31:57 +0800

    305-capitalize

--- solutions/305-capitalize/App.vue
+++ solutions/305-capitalize/App.vue
@@ -1,6 +1,16 @@
 <script setup>
-import { ref } from 'vue'
-const value = ref("")
+import { ref, vModelText } from 'vue';
+const value = ref('');
+
+vModelText.beforeUpdate = function (el, { value, modifiers: { capitalize } }) {
+  let newValue = value == null ? '' : value;
+  if (capitalize) {
+    newValue = newValue.charAt(0).toUpperCase() + value.slice(1);
+  }
+  if (el.value !== newValue) {
+    el.value = newValue;
+  }
+};
 </script>
 
 <template>
