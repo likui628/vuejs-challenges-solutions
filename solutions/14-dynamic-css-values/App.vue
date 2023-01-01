4713050b05eb34c4b5de525a240f65601b32004c
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-01 13:51:19 +0800

    14-dynamic-css-values

--- solutions/14-dynamic-css-values/App.vue
+++ solutions/14-dynamic-css-values/App.vue
@@ -1,13 +1,12 @@
 <script setup>
-import { ref } from "vue"
-const theme = ref("red")
+import { ref } from 'vue';
+const theme = ref('red');
 
-const colors = ["blue", "yellow", "red", "green"]
+const colors = ['blue', 'yellow', 'red', 'green'];
 
 setInterval(() => {
-  theme.value = colors[Math.floor(Math.random() * 4)]
-}, 1000)
-
+  theme.value = colors[Math.floor(Math.random() * 4)];
+}, 1000);
 </script>
 
 <template>
@@ -17,6 +16,6 @@ setInterval(() => {
 <style scoped>
 /* Modify the code to bind the dynamic color */
 p {
-  color: red
+  color: v-bind(theme);
 }
 </style>
