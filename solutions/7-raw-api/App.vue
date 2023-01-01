19cfe73f3959d13c206cd25d4115787aac305874
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-01 15:10:26 +0800

    7-raw-api

--- solutions/7-raw-api/App.vue
+++ solutions/7-raw-api/App.vue
@@ -1,22 +1,21 @@
 <script setup lang="ts">
-import { reactive, isReactive } from "vue"
+import { reactive, isReactive, toRaw, markRaw } from 'vue';
 
-const state = { count: 1 }
-const reactiveState = reactive(state)
+const state = { count: 1 };
+const reactiveState = reactive(state);
 
 /**
  * Modify the code so that we can make the output be true.
-*/
-console.log(reactiveState === state)
+ */
+console.log(toRaw(reactiveState) === state);
 
 /**
  * Modify the code so that we can make the output be false.
-*/
-const info = { count: 1 }
-const reactiveInfo = reactive(info)
+ */
+const info = markRaw({ count: 1 });
+const reactiveInfo = reactive(info);
 
-console.log(isReactive(reactiveInfo))
-
+console.log(isReactive(reactiveInfo));
 </script>
 
 <template>
