ff13f25d9adca0bdd2347550af807bef57073bc5
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-01 16:05:36 +0800

    8-effect-scope

--- solutions/8-effect-scope/App.vue
+++ solutions/8-effect-scope/App.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
-import { ref, computed, watch, watchEffect } from "vue"
+import { ref, computed, watch, watchEffect, effectScope } from 'vue';
 
-const counter = ref(1)
-const doubled = computed(() => counter.value * 2)
+const counter = ref(1);
+const doubled = computed(() => counter.value * 2);
 
 // use the `effectScope` API to make these effects stop together after being triggered once
+const scope = effectScope();
 
-watch(doubled, () => console.log(doubled.value))
-watchEffect(() => console.log(`Count: ${doubled.value}`))
+scope.run(() => {
+  watch(doubled, () => console.log(doubled.value));
+  watchEffect(() => console.log(`Count: ${doubled.value}`));
+});
 
-counter.value = 2
+counter.value = 2;
 
 setTimeout(() => {
-  counter.value = 4
-})
-
+  counter.value = 4;
+  scope.stop();
+});
 </script>
 
 <template>
