5938ba2be20ad669f2a0a2c21454baa7176946fb
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-02 20:05:21 +0800

    218-h-render-function

--- solutions/218-h-render-function/MyButton.ts
+++ solutions/218-h-render-function/MyButton.ts
@@ -1,8 +1,18 @@
-import { defineComponent } from "vue"
+import { defineComponent, h } from 'vue';
 
 export default defineComponent({
   name: 'MyButton',
+  emits: ['custom-click'],
   render() {
-    return h(/** do someting */)
-  }
-})
+    return h(
+      'button',
+      {
+        disabled: this.disabled,
+        onClick: () => {
+          this.$emit('custom-click');
+        },
+      },
+      this.$slots
+    );
+  },
+});
