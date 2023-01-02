d441a40dcc156f74037b472dfd60710df80aeafd
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-02 18:22:20 +0800

    21-functional-component

--- solutions/21-functional-component/App.vue
+++ solutions/21-functional-component/App.vue
@@ -1,35 +1,45 @@
-<script setup lang='ts'>
-
-import { ref } from "vue"
+<script setup lang="ts">
+import { ref, h } from 'vue';
 
 /**
  * Implement a functional component :
  * 1. Render the list elements (ul/li) with the list data
  * 2. Change the list item text color to red when clicked.
-*/
-const ListComponent = () => {
-}
+ */
+const ListComponent = (props, { slots, emit, attrs }) => {
+  const children = props.list.map((item, index) =>
+    h(
+      'li',
+      {
+        key: index,
+        style: props['active-index'] === index ? { color: 'red' } : undefined,
+        onClick: () => emit('toggle', index),
+      },
+      item.name
+    )
+  );
+  return h('ul', {}, children);
+};
 
-const list = [{
-  name: "John",
-}, {
-  name: "Doe",
-}, {
-  name: "Smith",
-}]
+const list = [
+  {
+    name: 'John',
+  },
+  {
+    name: 'Doe',
+  },
+  {
+    name: 'Smith',
+  },
+];
 
-const activeIndex = ref(0)
+const activeIndex = ref(0);
 
 function toggle(index: number) {
-  activeIndex.value = index
+  activeIndex.value = index;
 }
-
 </script>
 
 <template>
-  <list-component
-    :list="list"
-    :active-index="activeIndex"
-    @toggle="toggle"
-  />
+  <list-component :list="list" :active-index="activeIndex" @toggle="toggle" />
 </template>
