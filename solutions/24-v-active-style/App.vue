73b0cfe92e4c34f0026660aa79b1859163a31cd1
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-04 20:18:28 +0800

    24-v-active-style

--- solutions/24-v-active-style/App.vue
+++ solutions/24-v-active-style/App.vue
@@ -1,30 +1,35 @@
-<script setup lang='ts'>
-
-import { ref } from "vue"
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue';
 
 /**
  * Implement the custom directive
  * Make sure the list item text color changes to red when the `toggleTab` is toggled
  *
-*/
+ */
 const VActiveStyle = {
+  mounted(el, { value: [style, fn] }, vnode, prevVnode) {
+    watchEffect(() => {
+      const active = fn();
+      for (const [key, value] of Object.entries(style)) {
+        el.style[key] = active ? value : '';
+      }
+    });
+  },
+};
 
-}
-
-const list = [1, 2, 3, 4, 5, 6, 7, 8]
-const activeTab = ref(0)
+const list = [1, 2, 3, 4, 5, 6, 7, 8];
+const activeTab = ref(0);
 function toggleTab(index: number) {
-  activeTab.value = index
+  activeTab.value = index;
 }
-
 </script>
 
 <template>
   <ul>
     <li
-      v-for="(item,index) in list"
+      v-for="(item, index) in list"
       :key="index"
-      v-active-style="[{'color':'red'},() => activeTab === index]"
+      v-active-style="[{ color: 'red' }, () => activeTab === index]"
       @click="toggleTab(index)"
     >
       {{ item }}
