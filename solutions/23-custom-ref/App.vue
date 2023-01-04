12d8beca545491664585fe33c6868d581b04114c
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-04 20:58:32 +0800

    23-custom-ref

--- solutions/23-custom-ref/App.vue
+++ solutions/23-custom-ref/App.vue
@@ -1,20 +1,34 @@
 <script setup>
-import { watch } from "vue"
+import { watch, customRef } from 'vue';
 
 /**
  * Implement the function
-*/
-function useDebouncedRef(value, delay = 200) {
+ */
 
+function useDebouncedRef(value, delay = 200) {
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        track();
+        return value;
+      },
+      set(newValue) {
+        setTimeout(() => {
+          value = newValue;
+          trigger();
+        }, delay);
+      },
+    };
+  });
 }
-const text = useDebouncedRef("hello")
+const text = useDebouncedRef('hello');
 
 /**
  * Make sure the callback only gets triggered once when entered multiple times in a certain timeout
-*/
+ */
 watch(text, (value) => {
-  console.log(value)
-})
+  console.log(value);
+});
 </script>
 
 <template>
