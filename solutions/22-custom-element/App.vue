c2978943a186dc8a44e20475f9d9d028a354e2b0
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-05 17:29:30 +0800

    22-custom-element

--- solutions/22-custom-element/App.vue
+++ solutions/22-custom-element/App.vue
@@ -1,17 +1,25 @@
-<script setup lang='ts'>
-
-import { onMounted } from "vue"
+<script setup lang="ts">
+import { onMounted, defineCustomElement } from 'vue';
 
 /**
  * Implement the code to create a custom element.
  * Make the output of page show "Hello Vue.js".
-*/
-const VueJs = "???"
+ */
+const VueJs = defineCustomElement({
+  /* component options */
+  props: {
+    message: String,
+  },
+  template: `<span>{{message}}</span>`,
+});
+
+// Register the custom element.
+customElements.define('vue-js', VueJs);
 
 onMounted(() => {
-  document.getElementById("app")!.innerHTML = "<vue-js message=\"Hello Vue.js\"></vue-js>"
-})
-
+  document.getElementById('app')!.innerHTML =
+    '<vue-js message="Hello Vue.js"></vue-js>';
+});
 </script>
 
 <template>
