531615cffc01fa4f5df3d3832b8e4b3a14fbb909
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-05 17:18:17 +0800

    208-tree-component

--- solutions/208-tree-component/TreeComponent.vue
+++ solutions/208-tree-component/TreeComponent.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 interface TreeData {
-  key: string
-  title: string
-  children: TreeData[]
+  key: string;
+  title: string;
+  children: TreeData[];
 }
-defineProps<{data: TreeData[]}>()
+defineProps<{ data: TreeData[] }>();
 </script>
 
 <template>
   <!-- do something.... -->
+  <template v-for="item in data">
+    {{ item.title
+    }}<template v-if="item.children">
+      <TreeComponent :data="item.children" />
+    </template>
+  </template>
 </template>
